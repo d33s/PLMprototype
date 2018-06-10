0a31a4ed40487ec4af173dc8270676e5d89995a3
David B <david15@interia.eu>
2018-06-10 19:10:19 +0200

    app configuration for bean autowiring

--- src/main/java/com/enterprisesys/plm/repository/CustomersRepo.java
+++ src/main/java/com/enterprisesys/plm/repository/CustomersRepo.java
@@ -1,13 +1,13 @@
 package com.enterprisesys.plm.repository;
 
-        import com.enterprisesys.plm.model.Customer;
+import com.enterprisesys.plm.model.Customer;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Transactional
-@Component
+@Repository("customersRepo")
 public interface CustomersRepo extends CrudRepository<Customer, Integer> {
 
 }
